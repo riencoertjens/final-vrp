a407082768fd98fdb8f95446d5226d74196c520b
Rien Coertjens <rien.coertjens@gmail.com>
2019-06-07 20:08:16 +0200

    disable activity pages for now

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,36 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// /**
+//  * Implement Gatsby's Node APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.org/docs/node-apis/
+//  */
 
-const path = require("path")
+// const path = require("path")
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  return graphql(`
-    {
-      allWordpressWpActivities {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-    }
-  `).then(result => {
-    result.data.allWordpressWpActivities.edges.forEach(({ node }) => {
-      createPage({
-        path: `/activiteiten/${node.slug}`,
-        component: path.resolve(
-          `./src/components/templates/page-activity-template.js`
-        ),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          slug: node.slug,
-        },
-      })
-    })
-  })
-}
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   return graphql(`
+//     {
+//       allWordpressWpActivities {
+//         edges {
+//           node {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     result.data.allWordpressWpActivities.edges.forEach(({ node }) => {
+//       createPage({
+//         path: `/activiteiten/${node.slug}`,
+//         component: path.resolve(
+//           `./src/components/templates/page-activity-template.js`
+//         ),
+//         context: {
+//           // Data passed to context is available
+//           // in page queries as GraphQL variables.
+//           slug: node.slug,
+//         },
+//       })
+//     })
+//   })
+// }
