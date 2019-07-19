1606b2e925eb371c69f70d9e6ec61e908173289a
Rien Coertjens <rien.coertjens@gmail.com>
2019-07-19 21:32:08 +0200

    rm gatsby-source-wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,48 +28,48 @@ module.exports = {
     },
 
     // https://public-api.wordpress.com/wp/v2/sites/gatsbyjsexamplewordpress.wordpress.com/pages/
-    {
-      resolve: `gatsby-source-wordpress`,
-      options: {
-        /*
-         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
-         * Example : 'gatsbyjswpexample.wordpress.com' or 'www.example-site.com'
-         */
-        baseUrl: `${process.env.WP_URL}`,
-        // baseUrl: `www.vrp.be`,
-        // The protocol. This can be http or https.
-        protocol: `${process.env.WP_PROTOCOL}`,
-        // Set how many simultaneous requests are sent at once.
-        concurrentRequests: 10,
-        // protocol: `https`,
-        // Indicates whether the site is hosted on wordpress.com.
-        // If false, then the asumption is made that the site is self hosted.
-        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
-        // If your site is hosted on wordpress.org, then set this to false.
-        hostingWPCOM: false,
-        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
-        // This feature is untested for sites hosted on Wordpress.com
-        useACF: true,
-        auth: {
-          htaccess_user: `${process.env.WP_USER}`,
-          htaccess_pass: `${process.env.WP_PASS}`,
-          htaccess_sendImmediately: false,
-        },
-        plugins: [
-          {
-            resolve: `gatsby-wordpress-inline-images`,
-            options: {
-              baseUrl: `${process.env.WP_URL}`,
-              protocol: `${process.env.WP_PROTOCOL}`,
-            },
-          },
-        ],
-        // searchAndReplaceContentUrls: {
-        //   sourceUrl: "webhart.one",
-        //   replacementUrl: "vrp-final.netlify.com",
-        // },
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-wordpress`,
+    //   options: {
+    //     /*
+    //      * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+    //      * Example : 'gatsbyjswpexample.wordpress.com' or 'www.example-site.com'
+    //      */
+    //     baseUrl: `${process.env.WP_URL}`,
+    //     // baseUrl: `www.vrp.be`,
+    //     // The protocol. This can be http or https.
+    //     protocol: `${process.env.WP_PROTOCOL}`,
+    //     // Set how many simultaneous requests are sent at once.
+    //     concurrentRequests: 10,
+    //     // protocol: `https`,
+    //     // Indicates whether the site is hosted on wordpress.com.
+    //     // If false, then the asumption is made that the site is self hosted.
+    //     // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+    //     // If your site is hosted on wordpress.org, then set this to false.
+    //     hostingWPCOM: false,
+    //     // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+    //     // This feature is untested for sites hosted on Wordpress.com
+    //     useACF: true,
+    //     auth: {
+    //       htaccess_user: `${process.env.WP_USER}`,
+    //       htaccess_pass: `${process.env.WP_PASS}`,
+    //       htaccess_sendImmediately: false,
+    //     },
+    //     plugins: [
+    //       {
+    //         resolve: `gatsby-wordpress-inline-images`,
+    //         options: {
+    //           baseUrl: `${process.env.WP_URL}`,
+    //           protocol: `${process.env.WP_PROTOCOL}`,
+    //         },
+    //       },
+    //     ],
+    //     // searchAndReplaceContentUrls: {
+    //     //   sourceUrl: "webhart.one",
+    //     //   replacementUrl: "vrp-final.netlify.com",
+    //     // },
+    //   },
+    // },
 
     // build & transform
     `gatsby-transformer-json`,
