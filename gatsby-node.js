11e4c006803e82a3d743605df8cc32c55ad292e7
Rien Coertjens <rien.coertjens@gmail.com>
2019-07-19 18:19:37 +0200

    gatsby-node to json

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,171 +10,157 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return graphql(`
     {
-      activities: allWordpressWpActivities {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-      themas: allWordpressWpThema {
-        edges {
-          node {
-            slug
-            wordpress_id
-          }
-        }
-      }
-      ruimte: allWordpressWpRuimte {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-      artikels: allWordpressWpRuimteArtikel(
-        sort: {
-          fields: [acf___ruimte___acf___datum_publicatie, slug]
-          order: [ASC, ASC]
-        }
+      activities: allCollectionsJson(
+        filter: { post_type: { eq: "activity" } }
       ) {
         edges {
           node {
+            slug: post_name
+          }
+        }
+      }
+
+      themas: allTermsJson(filter: { taxonomy: { eq: "thema" } }) {
+        edges {
+          node {
             slug
-            title
+          }
+        }
+      }
+
+      ruimte: allCollectionsJson(filter: { post_type: { eq: "ruimte" } }) {
+        edges {
+          node {
+            slug: post_name
+          }
+        }
+      }
+
+      artikels: allCollectionsJson(
+        filter: { post_type: { eq: "ruimte_artikel" } }
+        sort: { fields: [acf___ruimte___acf___datum_publicatie, post_name] }
+      ) {
+        edges {
+          node {
+            slug: post_name
+            title: post_title
             acf {
               ruimte {
-                slug: post_name
+                ruimteSlug: post_name
               }
             }
           }
         }
       }
-      pages: allWordpressPage(
-        filter: { parent_element: { slug: { eq: "home" } } }
+
+      pages: allCollectionsJson(
+        filter: {
+          post_type: { eq: "page" }
+          post_parent: { post_name: { eq: "home" } }
+        }
       ) {
         edges {
           node {
-            slug
+            slug: post_name
           }
         }
       }
-      categories: allWordpressCategory(
+
+      categories: allTermsJson(
         filter: {
-          parent_element: { slug: { in: ["activiteiten", "prijzen"] } }
+          taxonomy: { eq: "category" }
+          parent_term: { in: ["activiteiten", "prijzen"] }
         }
-        sort: { fields: [parent_element___name, name], order: DESC }
       ) {
         edges {
           node {
             name
             slug
-            parent_element {
-              slug
-            }
+            parent_term
           }
         }
       }
     }
   `).then(result => {
-    result.data.activities.edges.forEach(({ node }) => {
-      createPage({
-        path: `/activiteit/${node.slug}`,
-        component: path.resolve(
-          `./src/components/templates/pages/activiteit-template.js`
-        ),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          slug: node.slug,
-        },
-      })
-    })
-    result.data.ruimte.edges.forEach(({ node }) => {
-      createPage({
-        path: `/ruimte/${node.slug}`,
-        component: path.resolve(
-          `./src/components/templates/pages/ruimte-template.js`
-        ),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          slug: node.slug,
-        },
-      })
-    })
-    const artikels = result.data.artikels.edges
-
-    artikels.forEach(({ node }, i) => {
-      createPage({
-        path: `/ruimte/${node.acf.ruimte.slug}/${node.slug}`,
-        component: path.resolve(
-          `./src/components/templates/pages/artikel-template.js`
-        ),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          slug: node.slug,
-          ruimteSlug: node.acf.ruimte.slug,
-          prev: i === 0 ? null : artikels[i - 1],
-          next: i === artikels.length - 1 ? null : artikels[i + 1],
-        },
-      })
-    })
-
-    result.data.themas.edges.forEach(({ node }) => {
-      createPage({
-        path: `/themas/${node.slug}`,
-        component: path.resolve(
-          `./src/components/templates/pages/thema-template.js`
-        ),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          slug: node.slug,
-          id: node.wordpress_id,
-        },
-      })
-    })
-
-    result.data.pages.edges.forEach(({ node }) => {
-      createPage({
-        path: `/${node.slug}`,
-        component: path.resolve(
-          `./src/components/templates/pages/wp-page-template.js`
-        ),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          slug: node.slug,
-        },
-      })
-    })
-
-    result.data.categories.edges.forEach(({ node }) => {
-      createPage({
-        path: `/${node.parent_element.slug}/${node.slug}`,
-        component: path.resolve(
-          `./src/components/templates/pages/category-template.js`
-        ),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          slug: node.slug,
-        },
-      })
-    })
-    // create extra page for the start@vrp category
-    createPage({
-      path: `/startvrp`,
-      component: path.resolve(
-        `./src/components/templates/pages/category-template.js`
-      ),
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: `/startvrp`,
-      },
-    })
+    // result.data.activities.edges.forEach(({ node }) => {
+    //   createPage({
+    //     path: `/activiteit/${node.slug}`,
+    //     component: path.resolve(
+    //       `./src/components/templates/pages/activiteit-template.js`
+    //     ),
+    //     context: {
+    //       slug: node.slug,
+    //     },
+    //   })
+    // })
+    // result.data.ruimte.edges.forEach(({ node }) => {
+    //   createPage({
+    //     path: `/ruimte/${node.slug}`,
+    //     component: path.resolve(
+    //       `./src/components/templates/pages/ruimte-template.js`
+    //     ),
+    //     context: {
+    //       slug: node.slug,
+    //     },
+    //   })
+    // })
+    // const artikels = result.data.artikels.edges
+    // artikels.forEach(({ node }, i) => {
+    //   createPage({
+    //     path: `/ruimte/${node.acf.ruimte.slug}/${node.slug}`,
+    //     component: path.resolve(
+    //       `./src/components/templates/pages/artikel-template.js`
+    //     ),
+    //     context: {
+    //       slug: node.slug,
+    //       ruimteSlug: node.acf.ruimte.slug,
+    //       prev: i === 0 ? null : artikels[i - 1],
+    //       next: i === artikels.length - 1 ? null : artikels[i + 1],
+    //     },
+    //   })
+    // })
+    // result.data.themas.terms.forEach(({ node: { slug } }) => {
+    //   createPage({
+    //     path: `/themas/${slug}`,
+    //     component: path.resolve(
+    //       `./src/components/templates/pages/thema-template.js`
+    //     ),
+    //     context: {
+    //       slug: slug,
+    //     },
+    //   })
+    // })
+    // result.data.pages.edges.forEach(({ node }) => {
+    //   createPage({
+    //     path: `/${node.slug}`,
+    //     component: path.resolve(
+    //       `./src/components/templates/pages/wp-page-template.js`
+    //     ),
+    //     context: {
+    //       slug: node.slug,
+    //     },
+    //   })
+    // })
+    // result.data.categories.edges.forEach(({ node }) => {
+    //   createPage({
+    //     path: `/${node.parent_term}/${node.slug}`,
+    //     component: path.resolve(
+    //       `./src/components/templates/pages/category-template.js`
+    //     ),
+    //     context: {
+    //       slug: node.slug,
+    //     },
+    //   })
+    // })
+    // // create extra page for the start@vrp category
+    // createPage({
+    //   path: `/startvrp`,
+    //   component: path.resolve(
+    //     `./src/components/templates/pages/category-template.js`
+    //   ),
+    //   context: {
+    //     slug: `/startvrp`,
+    //   },
+    // })
   })
 }
