f92fb08da1321775565c1efb7c7d7ab1fbf5d396
Rien Coertjens <rien.coertjens@gmail.com>
2019-07-19 21:33:15 +0200

    style-functions update for wpJson

--- src/components/webhart-components/style-functions.js
+++ src/components/webhart-components/style-functions.js
@@ -1,16 +1,16 @@
 export const MqMin = bp => `@media (min-width: ${bp})`
 export const MqMax = bp => `@media (max-width: ${bp})`
 
-export const getShowImage = (featured_media, ratio) => {
-  return featured_media && featured_media.localFile
-    ? featured_media.localFile.image.maxWidth.aspectRatio > ratio
-      ? featured_media.localFile.image.maxWidth
-      : featured_media.localFile.image.maxHeight
+export const getShowImage = (image, ratio) => {
+  return image && image.file
+    ? image.file.image.maxWidth.aspectRatio > ratio
+      ? image.file.image.maxWidth
+      : image.file.image.maxHeight
     : false
 }
 
 export const getCropFocus = smartcrop_image_focus => {
-  return smartcrop_image_focus.length > 0
-    ? `${smartcrop_image_focus[0].left}% ${smartcrop_image_focus[0].top}%`
+  return smartcrop_image_focus && smartcrop_image_focus.length > 0
+    ? `${smartcrop_image_focus.left}% ${smartcrop_image_focus.top}%`
     : "50% 50%"
 }
