38bea717e3928d7b854f1ae6ab8b9451f01fac33
Rien Coertjens <rien.coertjens@gmail.com>
2019-07-22 17:48:28 +0200

    Update artikel-template.js

--- src/components/templates/pages/artikel-template.js
+++ src/components/templates/pages/artikel-template.js
@@ -1,40 +1,38 @@
 import React from "react"
 import Layout from "../../Layout"
-// import { graphql } from "gatsby"
-// import { AspectRatioImage, AspectRatioBox } from "../../webhart-components"
-// import css from "@emotion/css"
+import { graphql } from "gatsby"
+import { AspectRatioImage } from "../../webhart-components"
+import css from "@emotion/css"
 // import GatsbyImage from "gatsby-image"
-// import GatsbyLink from "gatsby-link"
-// import {
-//   FaAngleRight as ArrowRightIcon,
-//   FaAngleLeft as ArrowLeftIcon,
-// } from "react-icons/fa"
+import GatsbyLink from "gatsby-link"
+import {
+  FaAngleRight as ArrowRightIcon,
+  FaAngleLeft as ArrowLeftIcon,
+} from "react-icons/fa"
 // import SEO from "../../webhart-components/SEO"
-// import BreadCrumbs from "../../BreadCrumbs"
+import BreadCrumbs from "../../BreadCrumbs"
 
-const ArtikelPageTemplate = () => {
-  // const ArtikelPageTemplate = ({
-  //   data: {
-  //     artikel: {
-  //       featured_media,
-  //       title: artikelTitle,
-  //       slug,
-  //       acf: {
-  //         beschrijving,
-  //         pdf_thumb: {
-  //           localFile: {
-  //             publicURL: pdf_thumb_original,
-  //             childImageSharp: { fixed: pdf_thumb },
-  //           },
-  //         },
-  //         pdf,
-  //         ruimte,
-  //       },
-  //     },
-  //   },
-  //   pageContext: { next, prev },
-  // }) => {
-
+const ArtikelPageTemplate = ({
+  data: {
+    artikel: {
+      featured_media,
+      title: artikelTitle,
+      // slug,
+      acf: {
+        //   beschrijving,
+        //   pdf_thumb: {
+        //     localFile: {
+        //       publicURL: pdf_thumb_original,
+        //       childImageSharp: { fixed: pdf_thumb },
+        //     },
+        //   },
+        //   pdf,
+        ruimte,
+      },
+    },
+  },
+  pageContext: { next, prev },
+}) => {
   // const title = `${ruimte.post_title}: ${artikelTitle}`
 
   return (
@@ -46,16 +44,9 @@ const ArtikelPageTemplate = () => {
         image={
           featured_media && featured_media.SEOImage.childImageSharp.SEO.src
         }
-      />
-      {featured_media ? (
+       />  */}
+      {featured_media && (
         <AspectRatioImage ratio={1200 / 630} image={featured_media} />
-      ) : (
-        <AspectRatioBox
-          ratio={1200 / 630}
-          css={css`
-            background: grey;
-          `}
-        />
       )}
       <BreadCrumbs
         crumbs={[
@@ -72,7 +63,7 @@ const ArtikelPageTemplate = () => {
           },
         ]}
       />
-      <section>
+      {/* <section>
         <h1>{title}</h1>
         <div
           css={css`
@@ -92,82 +83,82 @@ const ArtikelPageTemplate = () => {
             <span>open pdf</span>
           </a>
           <p>{beschrijving}</p>
-        </div>
-        <div
-          css={css`
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
-            a {
-            }
-          `}
-        >
-          {prev && (
-            <GatsbyLink
-              css={css`
-                margin-right: auto;
-              `}
-              to={`/ruimte/${prev.node.acf.ruimte.ruimteSlug}/${prev.node.slug}/`}
-            >
-              <ArrowLeftIcon />
-              {prev.node.title}
-            </GatsbyLink>
-          )}
-          {next && (
-            <GatsbyLink
-              css={css`
-                margin-left: auto;
-              `}
-              to={`/ruimte/${next.node.acf.ruimte.ruimteSlug}/${next.node.slug}/`}
-            >
-              {next.node.title}
-              <ArrowRightIcon />
-            </GatsbyLink>
-          )}
-        </div>
-      </section> */}
+        </div> */}
+      <div
+        css={css`
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          a {
+          }
+        `}
+      >
+        {prev && (
+          <GatsbyLink
+            css={css`
+              margin-right: auto;
+            `}
+            to={`/ruimte/${prev.node.acf.ruimte.ruimteSlug}/${prev.node.slug}/`}
+          >
+            <ArrowLeftIcon />
+            {prev.node.title}
+          </GatsbyLink>
+        )}
+        {next && (
+          <GatsbyLink
+            css={css`
+              margin-left: auto;
+            `}
+            to={`/ruimte/${next.node.acf.ruimte.ruimteSlug}/${next.node.slug}/`}
+          >
+            {next.node.title}
+            <ArrowRightIcon />
+          </GatsbyLink>
+        )}
+      </div>
+      {/* </section> */}
     </Layout>
   )
 }
 
 export default ArtikelPageTemplate
 
-// export const query = graphql`
-//   query($slug: String!) {
-//     artikel: collectionsJson(
-//       post_type: { eq: "ruimte_artikel" }
-//       post_name: { eq: $slug }
-//     ) {
-//       title: post_title
-//       slug: post_name
-//       featured_media: featured_img {
-//         ...HeroImageFragment
-//         SEOImage: file {
-//           ...SEOImageFragment
-//         }
-//       }
-//       acf {
-//         beschrijving
-//         ruimte {
-//           post_title
-//           post_name
-//         }
-//         pdf {
-//           url {
-//             publicURL
-//           }
-//         }
-//         pdf_thumb {
-//           localFile: url {
-//             publicURL
-//             childImageSharp {
-//               fixed(width: 300) {
-//                 ...GatsbyImageSharpFixed
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query($slug: String!) {
+    artikel: collectionsJson(
+      post_type: { eq: "ruimte_artikel" }
+      post_name: { eq: $slug }
+    ) {
+      title: post_title
+      slug: post_name
+      featured_media: featured_img {
+        ...HeroImageFragment
+        # SEOImage: file {
+        #   ...SEOImageFragment
+        # }
+      }
+      acf {
+        beschrijving
+        ruimte {
+          post_title
+          post_name
+        }
+        # pdf {
+        #   url {
+        #     publicURL
+        #   }
+        # }
+        # pdf_thumb {
+        #   localFile: url {
+        #     publicURL
+        #     childImageSharp {
+        #       fixed(width: 300) {
+        #         ...GatsbyImageSharpFixed
+        #       }
+        #     }
+        #   }
+        # }
+      }
+    }
+  }
+`
