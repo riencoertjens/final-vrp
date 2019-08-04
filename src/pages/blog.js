5f6a6043d21b779faa6e63122ffc343f56374c87
Rien Coertjens <rien.coertjens@gmail.com>
2019-08-04 16:57:21 +0200

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Layout from "../components/Layout"
-import PostList from "../components/PostList"
+import GatsbyLink from "gatsby-link"
+import css from "@emotion/css"
+import { AspectRatioImage, MediaQuery } from "../components/webhart-components"
+import { colors } from "../site/styles"
 
 const BlogPage = () => {
   return (
@@ -11,7 +14,13 @@ const BlogPage = () => {
           posts: allCollectionsJson(filter: { post_type: { eq: "blog" } }) {
             edges {
               node {
-                ...PostListFragment
+                title: post_title
+                slug: post_name
+                excerpt: post_excerpt
+                post_date(formatString: "DD-MM-Y")
+                featured_img {
+                  ...BlockImageFragment
+                }
               }
             }
           }
@@ -21,7 +30,61 @@ const BlogPage = () => {
         <Layout>
           <section>
             <h1>Blog</h1>
-            <PostList posts={posts}></PostList>
+            <div
+              css={css`
+                margin: 0 -1rem -1rem;
+              `}
+            >
+              {posts.edges.map(({ node: post }, i) => (
+                <div
+                  css={css`
+                    display: flex;
+                    flex-wrap: wrap;
+                    border-top: 2px solid ${colors.blue};
+                    :nth-child(odd) {
+                      background: whitesmoke;
+                    }
+                    h3 {
+                      margin-top: 0;
+                    }
+                    a {
+                      text-decoration: none;
+                    }
+                  `}
+                  key={i}
+                >
+                  <AspectRatioImage
+                    css={css`
+                      max-height: 150px;
+                      ${MediaQuery[0]} {
+                        max-height: 250px;
+                        flex: 0 1 250px;
+                      }
+                    `}
+                    image={post.featured_img}
+                  ></AspectRatioImage>
+                  <div
+                    css={css`
+                      padding: 1rem;
+                      p {
+                        margin: 0;
+                      }
+                      flex: 1 0 350px;
+                    `}
+                  >
+                    <GatsbyLink to={`/blog/${post.slug}`}>
+                      <h3>{post.title}</h3>
+                    </GatsbyLink>
+                    <p>
+                      {post.excerpt.slice(0, 300)} [...]{" "}
+                      <GatsbyLink to={`/blog/${post.slug}`}>
+                        lees meer...
+                      </GatsbyLink>
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </section>
         </Layout>
       )}
