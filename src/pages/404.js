046b2645e5a85723f1ebb8bb5d193ff5fbe34484
Rien Coertjens <rien.coertjens@gmail.com>
2019-08-05 13:50:36 +0200

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
+import SEO from "../components/webhart-components/SEO"
+import GatsbyLink from "gatsby-link"
 
 class ErrorPage extends Component {
   render() {
     return (
       <Layout>
+        <SEO title="error: 404 page not found"></SEO>
         <section>
-          <h1>Error: 404 not found</h1>
-          <p>oopsiedaisy</p>
+          <h1>Error: 404 pagina niet gevonde</h1>
+          <p>sorry, deze pagina werd niet gevonden.</p>
+          <p>
+            Probeer de <GatsbyLink to="/search">zoekfunctie</GatsbyLink> van
+            deze site om de gewenste pagina terug te vinden.
+          </p>
         </section>
       </Layout>
     )
