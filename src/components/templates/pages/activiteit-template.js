44dd11701503a8d77441ba0326b364756640195b
Rien Coertjens <rien.coertjens@gmail.com>
2019-10-07 17:49:39 +0700

    Revert "fix seo image"
    This reverts commit 8f129410e7254790137ea28835fdeb11fc14e2e9.

--- src/components/templates/pages/activiteit-template.js
+++ src/components/templates/pages/activiteit-template.js
@@ -56,7 +56,6 @@ const ActivityPageTemplate = ({
         description={activity.excerpt}
         image={
           activity.featured_img &&
-          activity.featured_img.SEOImage.childImageSharp &&
           activity.featured_img.SEOImage.childImageSharp.SEO.src
         }
       />
